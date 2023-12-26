715f903385299af6d0413f79d8c4e7b469dcb9f8
jagruth <jagruth@stratbeans.com>
2023-12-27 02:31:51 +0530

    added structure for new class MagicalArenaGame and included base condtions

--- MagicalArenaGame.java
+++ MagicalArenaGame.java
@@ -1,6 +1,33 @@
-class  MagicalArenaGame {
+public class MagicalArenaGame {
+      private Player playerA;
+      private Player playerB;
 
-  public static void main(String args[]) {
-    System.out.println("Hello Java");
-  }
+      // Constructor to initialize players in the arena
+      public MagicalArenaGame(Player playerA, Player playerB) {
+            this.playerA = playerA;
+            this.playerB = playerB;
+      }
+
+      // Method to start the match in the arena
+      public void startMatch() {
+
+            Player attacker = (playerA.getHealth() < playerB.getHealth()) ? playerA : playerB;
+            Player defender = (attacker == playerA) ? playerB : playerA;
+
+            
+      }
+
+      
+
+      public static void main(String[] args) {
+            // Create players
+            Player playerA = new Player("Player A", 50, 10, 5);
+            Player playerB = new Player("Player B", 100, 5, 10);
+
+            // Create a new game
+            MagicalArenaGame game = new MagicalArenaGame(playerA, playerB);
+
+            // Start the game
+            game.startMatch();
+      }
 }
