fb43253a0a09a9ce90f79e487f2664f55f7065d4
jagruth <jagruth@stratbeans.com>
2023-12-27 00:45:15 +0530

    created structure of player class

---
+++ Player.java
@@ -0,0 +1,24 @@
+public class Player {
+      private int health;
+
+      private int attack;
+
+      private int strength;
+
+      public Player() {};
+
+      public Player(int health, int attack, int strength) {
+            this.health = health;
+            this.attack = attack;
+            this.strength = strength;
+      }
+
+      public void attack(Player player) {
+            // attack code
+      }
+
+
+      public void defened(Player player) {
+            // defend code
+      }
+}
